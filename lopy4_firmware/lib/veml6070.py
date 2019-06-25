fa1eeb35df5175e7581ba14eb71222299087b368
D Near <dan.e.near@gmail.com>
2019-06-25 16:26:11 -0500

    Create veml6070.py
    Library placeholder for Vishay VEML6070 code

---
+++ lopy4_firmware/lib/veml6070.py
@@ -0,0 +1,5 @@
+#placeholder for Vishay VEML6070 UV-A light sensor
+#this light sensor outputs UV-A sensor readings via the I2C bus
+#There's existing code for Arduino using this, so it shouldn't be hard to cross-compile it to python
+#datasheet located here https://www.vishay.com/docs/84310/designingveml6070.pdf
+
