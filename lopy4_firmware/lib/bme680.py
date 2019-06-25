ac3672cc46ccb2dac1a0ee4a3e208febf87ee60f
D Near <dan.e.near@gmail.com>
2019-06-25 16:23:16 -0500

    Create bme680.py
    Created library place holder for BME680 sensor code

---
+++ lopy4_firmware/lib/bme680.py
@@ -0,0 +1,5 @@
+#Library space reserved for Bosch BME680 sensor
+#this sensor outputs temperature, Humidity, Barometric Pressure and VOC (Volatile Organic Compounds) via the I2C bus
+#there's existing code for Arduino for this so it shouldn't be hard to cross-compile to Python
+#will link to it shortly.
+#Bosch BME680 datasheet https://ae-bst.resource.bosch.com/media/_tech/media/datasheets/BST-BME680-DS001.pdf
