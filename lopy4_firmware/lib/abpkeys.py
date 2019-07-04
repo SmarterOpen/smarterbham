167ddfc85224452ca0fa951dca46e9a5de0f04b1
D Near <dan.e.near@gmail.com>
2019-07-04 17:01:47 -0500

    Update abpkeys.py
    Added OTAA key parameters with blanked out values

--- lopy4_firmware/lib/abpkeys.py
+++ lopy4_firmware/lib/abpkeys.py
@@ -1,6 +1,11 @@
 # Don't store or commit keys here.  This file shown for others to know what it should contain to make this work.
 import struct
 import ubinascii
-dev_addr = struct.unpack(">l", ubinascii.unhexlify('00000000'))[0]
-nwk_swkey = ubinascii.unhexlify('00000000000000000000000000000000')
-app_swkey = ubinascii.unhexlify('00000000000000000000000000000000')
+#abp keys - not in use.. TTN prefers OTAA
+#dev_addr = struct.unpack(">l", ubinascii.unhexlify('00000000'))[0]
+#nwk_swkey = ubinascii.unhexlify('00000000000000000000000000000000')
+#app_swkey = ubinascii.unhexlify('00000000000000000000000000000000')
+# create an OTAA authentication parameters
+_dev_eui = ubinascii.unhexlify('0000000000000000')
+_app_eui = ubinascii.unhexlify('0000000000000000')#70B3D57ED0011AF7')
+_app_key = ubinascii.unhexlify('00000000000000000000000000000000')
