822a861a4bfa5ef13945f900a73a970b83842772
D Near <dan.e.near@gmail.com>
2019-07-05 21:06:11 -0500

    Create lorakeys.py

---
+++ lopy4_firmware/lib/lorakeys.py
@@ -0,0 +1,6 @@
+#store Lora OTAA keys here - but don't back this up to Github
+import struct
+import ubinascii
+_dev_eui = ubinascii.unhexlify('0000000000000000')
+_app_eui = ubinascii.unhexlify('0000000000000000')#70B3D57ED0011AF7')
+_app_key = ubinascii.unhexlify('00000000000000000000000000000000')
