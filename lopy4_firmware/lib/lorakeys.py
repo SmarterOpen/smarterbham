0f98a1919552f8169d7b8ccb9b12bfda34cd58ee
D Near <dan.e.near@gmail.com>
2019-07-06 11:19:58 -0500

    Update lorakeys.py

--- lopy4_firmware/lib/lorakeys.py
+++ lopy4_firmware/lib/lorakeys.py
@@ -2,5 +2,5 @@
 import struct
 import ubinascii
 _dev_eui = ubinascii.unhexlify('0000000000000000')
-_app_eui = ubinascii.unhexlify('0000000000000000')#70B3D57ED0011AF7')
+_app_eui = ubinascii.unhexlify('0000000000000000')
 _app_key = ubinascii.unhexlify('00000000000000000000000000000000')
