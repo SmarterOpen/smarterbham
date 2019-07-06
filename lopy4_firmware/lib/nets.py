bbbc654b46d707eab26ad1e7cc1eb7fc26d2a4bc
D Near <dan.e.near@gmail.com>
2019-07-06 15:30:34 -0500

    Create nets.py

---
+++ lopy4_firmware/lib/nets.py
@@ -0,0 +1,6 @@
+#Don't check this data into Github!!
+#code modified from https://docs.pycom.io/tutorials/all/wlan.html
+known_nets = {
+    '<net1>': {'pwd': '<passwrd>'},
+    '<net2>': {'pwd': '<passwrd>', 'wlan_config':  ('10.0.0.114', '255.255.0.0', '10.0.0.1', '10.0.0.1')}, # (ip, subnet_mask, gateway, DNS_server)
+}
